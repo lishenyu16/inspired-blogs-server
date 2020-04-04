eb509669280da79a50661a7a165ef673d5d38add
Shenyu <lishenyu16@gmail.com>
2020-04-04 13:38:27 -0700

    save on implementing forot password logic

--- routes/auth/Auth.js
+++ routes/auth/Auth.js
@@ -203,6 +203,123 @@ router.post('/confirmEmail', async (req, res) =>{
         })
     }
 })
+router.post('/forgotPassword', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const saltRounds = 10;
+        const queryExiting = `select * from accounts where email = $1`;
+        const queryUpdateHash = `update accounts set hash = $1, hash_expiration_time = $2 where email = $3`;
+        const result = await pool.query(queryExiting,[email]);
+        if (result.rows.length == 0){
+            res.status(200).json({
+                success: false,
+                message: 'Email not found.'
+            })
+        }
+        else {
+            let verificationCode = uuid();
+            const verificationHash = await bcrypt.hash(verificationCode, saltRounds);
+            const result_userId = await pool.query(queryUpdateHash,[verificationHash,new Date().getTime()+24*60*60*1000, email]);
+            transporter.sendMail({
+                to: email,
+                from: 'inspiredblogs@gmail.com',
+                subject: 'Inspired Blogs Reset Password',
+                html: process.env.NODE_ENV === "production"?
+                    `<h1>Please verify your email by clicking on the following link:</h1>
+                    <div style="width:100%;text-align:center;">
+                        <a style="padding:5px;background-color:cyan;text-decoration:none" 
+                        href="http://shenyu16.com/resetPassword/${verificationCode}/${result.rows[0].user_id}">Confirm Email Address</a>
+                    </div>
+                    <div style="width:100%;text-align:center;">
+                        or copy this link to your browser: <b>http://shenyu16.com/resetPassword/${verificationCode}/${result.rows[0].user_id}</b>
+                    </div>
+                    `
+                    :
+                    `<h1>Please verify your email by clicking on the following link:</h1>
+                    <div style="width:100%;text-align:center;">
+                        <a style="padding:5px;background-color:cyan;text-decoration:none" 
+                        href="http://localhost:8080/resetPassword/${verificationCode}/${result.rows[0].user_id}">Confirm Email Address</a>
+                    </div>
+                    <div style="width:100%;text-align:center;">
+                        or copy this link to your browser: <b>http://localhost:8080/resetPassword/${verificationCode}/${result.rows[0].user_id}</b>
+                    </div>
+                    `
+            })
+            .then(re=>{
+                console.log(re);
+                res.status(200).json({
+                    success: true,
+                    message: 'Email sent, check inbox link'
+                })
+            })
+            .catch(err=>{
+                console.log(err);
+                res.status(400).json({
+                    success: false,
+                    message: 'Something wrong happened to our server'
+                })
+            })
+        }
+    }
+    catch(e){
+        console.log(e);
+        res.status(400).json({
+            success: false,
+            message: 'Failed to sign up, please contact admin for help!'
+        })
+    }
+})
+router.post('/resetPassword', async (req, res) => {
+    try {
+        let verificationCode = req.body.verificationCode;
+        let userId = req.body.userId;
+        let password = req.body.password;
+        let pendinguser_sql = `select * from accounts where user_id = $1`;
+        let updatePw_sql = `update accounts set password = $1 where user_id = $2`;
+        const result = await pool.query(pendinguser_sql,[userId]);
+        if (result.rows.length > 0){
+            let user = result.rows[0];
+            let hashedCode = user.hash;
+            let hashExpirationTime = user.hash_expiration_time;
+            let currentTime = new Date().getTime();
+            if (currentTime <= hashExpirationTime){
+                const match = await bcrypt.compare(verificationCode, hashedCode);
+                if (match){
+                    await pool.query(updatePw_sql,[password,userId]);
+                    return res.status(200).json({
+                        success: true,
+                        message: 'successfully updated password'
+                    })
+                }
+                else {
+                    return res.status(401).json({
+                       success: false,
+                       message: 'Failed to verify email token: invalid verification token was submitted.'     
+                    })
+                }
+            }
+            else {
+                return res.status(401).json({
+                    success: false,
+                    message: 'verificaton code expired'
+                })
+            }
+        }
+        else {
+            return res.status(400).json({
+                success: false,
+                message: 'User not found.'
+            })
+        }
+    }
+    catch(err){
+        console.log(err);
+        return res.status(400).json({
+            success: false,
+            message: err
+        })
+    }
+})
 router.post('/updateProfile', isAuthMiddleware, async (req, res) => {
     const userId = req.userId;
     const username = req.body.username;
