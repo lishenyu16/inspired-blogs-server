fe728d4b4b442df9ce78dc3c881313009a0dcc30
Shenyu <lishenyu16@gmail.com>
2020-04-06 18:23:36 -0700

    fix a route of confirmation email

--- routes/blogs/staticRouterBlogs.js
+++ routes/blogs/staticRouterBlogs.js
@@ -7,6 +7,9 @@ router.get('/public-blogs', async (req,res) => {
     res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
 })
 
+router.get('/confirm-email/:code/:userId', async (req,res) => {
+    res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
+})
 router.get('/blog-detail/:blog_id', async (req,res) => {
     res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
 })
