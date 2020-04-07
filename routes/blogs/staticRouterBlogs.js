74379df5b2cf7c3a93dc0f50ba24904319aca3df
Shenyu <lishenyu16@gmail.com>
2020-04-06 18:14:24 -0700

    solved routing issues

---
+++ routes/blogs/staticRouterBlogs.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const appRoot = require('app-root-path');
+
+router.get('/public-blogs', async (req,res) => {
+    res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
+})
+
+router.get('/blog-detail/:blog_id', async (req,res) => {
+    res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
+})
+router.get('/categories', async (req,res) => {
+    res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
+})
+router.get('/profile', async (req,res) => {
+    res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
+})
+router.get('/login', async (req,res) => {
+    res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
+})
+router.get('/signup', async (req,res) => {
+    res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
+})
+router.get('/forgot-password', async (req,res) => {
+    res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
+})
+
+router.get('/', async (req,res) => {
+    res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
+})
+
+module.exports = router;
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 
 const authRouter = require('./auth/Auth');
+const blogsStaticRouter = require('./blogs/staticRouterBlogs');
 const blogsRouter = require('./blogs/blogs');
 const s3Router = require('./s3/fileTransfer');
 
 module.exports = (app) => {
+    app.use('/blogs', blogsStaticRouter);
     app.use('/api/auth', authRouter);
     app.use('/api/blogs', blogsRouter);
     app.use('/api/s3', s3Router);
