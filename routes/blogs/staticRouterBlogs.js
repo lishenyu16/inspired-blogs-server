d26203bdbb3e7d14de85feb39d4eeaf790f1c3e7
Shenyu <lishenyu16@gmail.com>
2020-04-06 18:40:04 -0700

    fix routes

--- routes/blogs/staticRouterBlogs.js
+++ routes/blogs/staticRouterBlogs.js
@@ -10,6 +10,9 @@ router.get('/public-blogs', async (req,res) => {
 router.get('/confirm-email/:code/:userId', async (req,res) => {
     res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
 })
+router.get('/reset-password/:code/:userId', async (req,res) => {
+    res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
+})
 router.get('/blog-detail/:blog_id', async (req,res) => {
     res.sendFile(path.join(`${appRoot}/views`, 'index.html'));
 })
